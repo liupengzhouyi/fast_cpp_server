d7af5b7dc042b801458865f9418d68a4284263fe
peng liu <pengliu@pengdeMacBook-Pro.local>
2025-02-16 20:19:55 +0800

    fix: Add unit test code.

---
+++ test/example.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include "example.h"
+
+TEST(ExampleTest, Add) {
+    Example example;
+    EXPECT_EQ(example.add(1, 1), 2);
+    EXPECT_EQ(example.add(-1, 1), 0);
+    EXPECT_EQ(example.add(-1, -1), -2);
+}
+
+TEST(ExampleTest, Subtract) {
+    Example example;
+    EXPECT_EQ(example.subtract(1, 1), 0);
+    EXPECT_EQ(example.subtract(-1, 1), -2);
+    EXPECT_EQ(example.subtract(-1, -1), 0);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
